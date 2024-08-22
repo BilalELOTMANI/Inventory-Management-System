1bd0f1069f4418c69dc3be7953d7d1081a91d05d
BilalELOTMANI <bilalelotmani100@gmail.com>
2024-08-22 23:29:10 +0200

    Update file

---
+++ CustomerButton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_Management_System
+{
+    public partial class CustomerButton : PictureBox
+    {
+        public CustomerButton()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomerButton_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private Image NormalImage;
+        private Image HoverImage;
+
+        public Image ImageNormal
+        {
+            get { return ImageNormal; }
+            set { ImageNormal = value;}
+        }
+
+        public Image ImageHover
+        {
+            get { return ImageHover; }
+            set { ImageHover = value; }
+        }
+
+        private void CustomerButton_MouseHover(object sender, EventArgs e)
+        {
+            this.Image = NormalImage;
+        }
+
+        private void CustomerButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.Image = HoverImage;
+        }
+    }
+}
